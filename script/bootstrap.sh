5668021bbcc68b4eef0ba09f039e1c2a89fd32a4
Adrian Hajdo <hajdoadrian01@gmail.com>
2025-01-03 10:28:42 +0000

    Install front-end dependencies
    Ensure system tests can run.
    Create script for installing other requirements when first installing the application.

---
+++ script/bootstrap.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -x
+
+bundle install
+rake db:create
+/usr/local/rvm/bin/rvm install 3.3.3
+
+sudo apt update
+sudo apt install --no-install-recommends -y nodejs npm
+sudo npm install -g yarn
+
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.1
+echo '. "~/.asdf/asdf.sh"' >> ~/.bashrc
+echo '. "~/.asdf/completions/asdf.bash"' >> ~/.bashrc
+
+source ~/.bashrc
+
+asdf plugin add nodejs
+asdf install nodejs latest
+
+rails turbo:install
+rails stimulus:install
+rails css:install:sass
+rails javascript:install:esbuild
+
+asdf plugin add yarn
+asdf install yarn latest
+
+asdf global nodejs latest
+asdf global yarn latest
+
+npm install --save-exact --save-dev esbuild
